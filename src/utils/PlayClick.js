458dfe604a94c78dca7acdd347bb1cf75a67c3af
Harsha <goggiharsha@gmail.com>
2026-07-28 10:40:12 +0530

    Add click sound utility

---
+++ src/utils/PlayClick.js
@@ -0,0 +1,12 @@
+const clickSound = new Audio("/sounds/click.mp3");
+
+clickSound.preload = "auto";
+clickSound.volume = 0.35;
+
+export function playClick() {
+  clickSound.currentTime = 0;
+
+  clickSound.play().catch(() => {
+    // Ignore autoplay errors
+  });
+}
